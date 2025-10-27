822f76a71b139c77394177a59e01699285c52238
artem777764 <artem200513286@gmail.com>
2025-10-27 22:50:57 +0300

    OrderService Swagger

--- src/OrderService/Extensions/BackendServiceExtensions.cs
+++ src/OrderService/Extensions/BackendServiceExtensions.cs
@@ -6,6 +6,16 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+            {
+                Title = "Defect Managment Project API",
+                Version = "v1"
+            });
+        });
+
         services.AddControllers()
             .AddJsonOptions(options =>
             {
@@ -22,6 +32,15 @@ public static class ServiceCollectionExtensions
     
     public static WebApplication ConfigureApplication(this WebApplication app)
     {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger(); // http://localhost:5007/swagger/index.html
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Defect Managment Project API");
+            });
+        }
+
         app.UseHttpsRedirection();
         app.UseRouting();
         app.MapControllers();
